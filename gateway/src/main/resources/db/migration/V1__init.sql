5f5087e01c10b862b06b1c960a85035d4bae8480
Adam Juraszek <adam.juraszek@ataccama.com>
2017-11-24 20:15:58 +0100

    Add volues, use flyway

--- gateway/src/main/resources/db/migration/V1__init.sql
+++ gateway/src/main/resources/db/migration/V1__init.sql
@@ -1,21 +1,26 @@
 CREATE TABLE solution (
 	id uuid NOT NULL,
-	code text NOT NULL,
-	created timestamp NOT NULL,
 	email varchar(255) NOT NULL,
-	grading_ended timestamp NULL,
-	grading_started timestamp NULL,
-	language varchar(255) NOT NULL,
-	message text NULL,
-	processing_ended timestamp NULL,
-	processing_started timestamp NULL,
-	"result" varchar(255) NOT NULL,
-	score int4 NULL,
 	task varchar(255) NOT NULL,
+	language varchar(255) NOT NULL,
+	code varchar(32768) NOT NULL,
+
+	created timestamp NOT NULL,
 	updated timestamp NOT NULL,
+
+	processing_started timestamp NULL,
+	processing_ended timestamp NULL,
+	processing_result varchar(255) NOT NULL,
+	processing_message varchar(32768) NULL,
+
+	grading_started timestamp NULL,
+	grading_ended timestamp NULL,
+	grading_result varchar(255) NOT NULL,
+	grading_score int4 NULL,
+	grading_message varchar(32768) NULL,
+
 	CONSTRAINT solution_pkey PRIMARY KEY (id)
 )
 WITH (
 	OIDS=FALSE
 ) ;
-
