fa2ac5f7f351acefdfbb598b0048a9ed843821af
Jinda, Martin <martin.jinda@ataccama.com>
2017-11-28 11:16:02 +0100

    Added GTM tracking code.

--- frontend/index.php
+++ frontend/index.php
@@ -41,6 +41,13 @@
 	<script src="./ezbsalert.js"></script>
 	<link rel="stylesheet" href="./main.css?v=1">
 	<script type="text/javascript" src="./script.js?v=1"></script>
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+       })(window,document,'script','dataLayer','GTM-MXGLQ3Z');</script>
+	<!-- End Google Tag Manager -->
 </head>
 
 <body>
